8d882dfbc2a467c8f3b46e05d407d11b82e9f6b7
Marcio <blogdomarcio@live.com>
2020-12-08 16:36:56 -0300

    Fix II

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "tailwindcss/tailwind.css"
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 
 const MyApp = ({ Component, pageProps }) => {
